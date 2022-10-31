0d673ec6723a630d3072ce7689813d12ac2667a8
김가영 <namiein@naver.com>
2022-10-31 22:34:06 +0900

    mod : volume은 버튼으로만 제어할 수 있도록 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable jsx-a11y/media-has-caption */
-import { ChangeEvent, FormEvent, useRef, useState } from 'react';
+import { ChangeEvent, useRef, useState } from 'react';
 
 import { PlayIcon, PauseIcon, ForwardIcon, BackwardIcon } from '@heroicons/react/24/solid';
 
@@ -60,17 +60,7 @@ export default function Home() {
 
     const onScreenClick = () => setShow((prev) => !prev);
 
-    // const handleVolume = (e: FormEvent<HTMLInputElement>) => {
-    //     if (show && audio.current) {
-    //         e.stopPropagation();
-    //         audio.current.load();
-    //         audio.current.volume = Number(e.currentTarget.value);
-    //         setVolume(Number(e.currentTarget.value));
-    //         setVolumeHeight(`calc(100% - ${Math.floor(parseFloat(e.currentTarget.value) * 100)}%)`);
-    //     }
-    // };
-
-    const handleVolumeUp = () => {
+    const onVolumeUp = () => {
         if (audio.current) {
             audio.current.load();
             const newVolume = volume === 100 ? volume : volume + 0.1;
@@ -81,7 +71,7 @@ export default function Home() {
         }
     };
 
-    const handleVolumeDown = () => {
+    const onVolumeDown = () => {
         if (audio.current) {
             audio.current.load();
             const newVolume = volume === 0 ? volume : volume - 0.1;
@@ -92,6 +82,8 @@ export default function Home() {
         }
     };
 
+    // TODO : When Music playing, show sound effect animation
+
     return (
         <main className="w-full min-h-screen flex justify-center items-center flex-col gap-2.5">
             <label htmlFor="file" className="w-[300px]">
@@ -100,16 +92,17 @@ export default function Home() {
             </label>
             <div className="relative w-[300px] h-[500px] bg-black rounded-md p-5">
                 <div className="absolute -right-[5px] flex flex-col gap-[0.5px]">
-                    <button className="w-[5px] h-[30px] bg-black" aria-label="volume up" title="volume up" type="button" onClick={handleVolumeUp} />
-                    <button className="w-[5px] h-[30px] bg-black" aria-label="volume down" title="volume down" type="button" onClick={handleVolumeDown} />
+                    <button className="w-[5px] h-[30px] bg-black" aria-label="volume up" title="volume up" type="button" onClick={onVolumeUp} />
+                    <button className="w-[5px] h-[30px] bg-black" aria-label="volume down" title="volume down" type="button" onClick={onVolumeDown} />
                 </div>
-                <div className="overflow-hidden w-full h-[150px] bg-blue-50 rounded-md border-2 border-black" role="button" tabIndex={-1} onKeyDown={onScreenClick} onClick={onScreenClick}>
-                    {current && current?.name}
+                <div className="overflow-hidden w-full h-[150px] bg-blue-50 rounded-md border-2 p-2.5 border-black" role="button" tabIndex={-1} onKeyDown={onScreenClick} onClick={onScreenClick}>
+                    <p className="text-[12px] font-semibold font-mono">{current && current?.name}</p>
                 </div>
                 {show && (
                     <label htmlFor="volume" className="overflow-hidden w-[20px] h-[100px] absolute top-[45px] right-[30px] bottom-[45px] bg-blue-50">
                         <div style={{ height: volumeHeight }} className="z-50 h-0 w-[17px] bg-blue-50 absolute top-[-1px] right-0" />
                         <input
+                            disabled
                             ref={volumeRef}
                             id="volume"
                             className="h-0 w-[99px] absolute top-[41px] right-[-41px] appearance-none focus:outline-none -rotate-90"
